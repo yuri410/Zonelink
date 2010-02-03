38ca2c9529497efdea7eabca4fb6ca182d8f3de5
yuri410 <canbitwell@gmail.com>
2010-02-03 07:15:44 +0000

    basic framework

---
+++ Source/Code2015/GUI/InGameUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apoc3D;
+using Apoc3D.Graphics;
+using Apoc3D.Vfs;
+using Code2015.EngineEx;
+
+namespace Code2015.GUI
+{
+    class InGameUI : UIComponent
+    {
+        GameScene scene;
+        RenderSystem renderSys;
+        Code2015 game;
+        Game parent;
+        Font font;
+
+        public InGameUI(Code2015 game,Game parent, GameScene scene)
+        {
+            this.parent = parent;
+
+            this.game = game;    
+            this.renderSys = game.RenderSystem;
+            this.scene = scene;
+
+            FileLocation fl = FileSystem.Instance.Locate("def.fnt", GameFileLocs.UI);
+            font = FontManager.Instance.CreateInstance(renderSys, fl, "default");
+        }
+
+        public override void Render(Sprite sprite)
+        {
+            if (!parent.IsLoaded)
+            {
+                font.DrawString(sprite, "Loading", 0, 0, 34, DrawTextFormat.Center, -1);
+            }
+        }
+
+        public override void Update(GameTime time)
+        {
+
+        }
+    }
+}
--- Source/Code2015/GUI/Menu.cs
+++ Source/Code2015/GUI/Menu.cs
@@ -4,13 +4,53 @@ using System.Linq;
 using System.Text;
 using Apoc3D;
 using Apoc3D.Graphics;
+using Apoc3D.Vfs;
+using Code2015.EngineEx;
+using XI = Microsoft.Xna.Framework.Input;
 
 namespace Code2015.GUI
 {
-    class Menu : IGameComponent
+    class Menu : UIComponent, IGameComponent
     {
-        public void Render() { }
-        public void Render(Sprite sprite) { }
-        public void Update(GameTime time) { }
+        Font font;
+
+        Code2015 game;
+
+        public Menu(Code2015 game, RenderSystem rs)
+        {
+            FileLocation fl = FileSystem.Instance.Locate("def.fnt", GameFileLocs.UI);
+            font = FontManager.Instance.CreateInstance(rs, fl, "default");
+            this.game = game;
+        }
+
+        public void Render()
+        {
+
+        }
+
+        public override void Render(Sprite sprite)
+        {
+            if (!game.IsIngame)
+            {
+                font.DrawString(sprite, "Not Implemented\nPress Enter to start a new game", 0, 0, 34, DrawTextFormat.Center, -1);
+            }
+        }
+
+        public override void Update(GameTime time)
+        {
+            if (!game.IsIngame)
+            {
+                XI.KeyboardState state = XI.Keyboard.GetState();
+
+                if (state.IsKeyDown(XI.Keys.Enter))
+                {
+                    GameCreationParameters gcp = new GameCreationParameters();
+
+                    game.StartNewGame(gcp);
+
+                    return;
+                }
+            }
+        }
     }
 }
