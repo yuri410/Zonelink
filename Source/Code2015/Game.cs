07e25efdc3ec4c250327e8bd2203a0e4caf20674
yuri410 <canbitwell@gmail.com>
2010-02-23 12:16:49 +0000

    city style table init

--- Source/Code2015/Game.cs
+++ Source/Code2015/Game.cs
@@ -92,21 +92,21 @@ namespace Code2015
 
             // 初始化场景
             this.scene = new GameScene(renderSys);
-            //this.cityStyles = new CityStyleTable(renderSys);
+            this.cityStyles = new CityStyleTable(renderSys);
 
-            //SimulationRegion slgSystem = gameState.SLGWorld;
-            //for (int i = 0; i < slgSystem.Count; i++)
-            //{
-            //    SimulateObject obj = slgSystem[i];
-            //    City city = obj as City;
+            SimulationRegion slgSystem = gameState.SLGWorld;
+            for (int i = 0; i < slgSystem.Count; i++)
+            {
+                SimulateObject obj = slgSystem[i];
+                City city = obj as City;
 
-            //    if (city != null)
-            //    {
-            //        CityObject cityObj = new CityObject(city, cityStyles);
+                if (city != null)
+                {
+                    CityObject cityObj = new CityObject(city, cityStyles);
 
-            //        scene.Scene.AddObjectToScene(cityObj);
-            //    }
-            //}
+                    scene.Scene.AddObjectToScene(cityObj);
+                }
+            }
 
           
             this.ingameUI = new InGameUI(game, this, scene);
