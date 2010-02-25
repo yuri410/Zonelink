f0e550e15e5231256988a10973f5852890d7ecb2
canbitwell <canbitwell@184c9104-dd7f-11de-b0fd-b3f6642ff6c2>
2010-02-25 08:10:35 +0000

    APOC3D EMBEDDED

---
+++ trunk/Source/Code2015/Effect.lpk/SkyboxEffect.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apoc3D.Graphics;
+using Apoc3D.Graphics.Effects;
+using Apoc3D.MathLib;
+
+namespace Code2015.Effects
+{
+    public class SkyboxEffectFactory : EffectFactory
+    {
+        static readonly string typeName = "Skybox";
+
+        public static string Name
+        {
+            get { return typeName; }
+        }
+
+
+        RenderSystem renderSystem;
+
+        public SkyboxEffectFactory(RenderSystem rs)
+        {
+            renderSystem = rs;
+        }
+
+        public override Effect CreateInstance()
+        {
+            return new WaterEffect(renderSystem);
+        }
+
+        public override void DestroyInstance(Effect fx)
+        {
+            fx.Dispose();
+        }
+    }
+
+    class SkyboxEffect : Effect
+    {
+        RenderSystem renderSystem;
+
+        PixelShader pixShader;
+        VertexShader vtxShader;
+
+        public SkyboxEffect(RenderSystem renderSystem)
+            : base(false, SkyboxEffectFactory.Name)
+        {
+            this.renderSystem = renderSystem;
+        }
+
+        protected override int begin()
+        {
+            renderSystem.BindShader(vtxShader);
+            renderSystem.BindShader(pixShader);
+
+            return 1;
+        }
+
+        protected override void end()
+        {
+            renderSystem.BindShader((VertexShader)null);
+            renderSystem.BindShader((PixelShader)null);
+        }
+
+        public override void BeginPass(int passId)
+        {
+        }
+
+        public override void EndPass()
+        {
+
+        }
+
+        //public override void BeginShadowPass()
+        //{
+        //}
+
+        //public override void EndShadowPass()
+        //{
+        //}
+
+        public override void Setup(Material mat, ref RenderOperation op)
+        {
+            pixShader.SetTexture("texColor", mat.GetTexture(0));
+            Matrix mvp = op.Transformation * EffectParams.CurrentCamera.ViewMatrix * EffectParams.CurrentCamera.ProjectionMatrix;
+
+            vtxShader.SetValue("mvp", ref mvp);
+        }
+
+        //public override void SetupShadowPass(Material mat, ref RenderOperation op)
+        //{
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            vtxShader.Dispose();
+            pixShader.Dispose();
+        }
+    }
+}
