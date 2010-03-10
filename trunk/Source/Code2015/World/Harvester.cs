dd1ff0417261373ede743a2fb47c22e823fa02d3
canbitwell <canbitwell@184c9104-dd7f-11de-b0fd-b3f6642ff6c2>
2010-03-10 09:11:29 +0000

    path finder impl. harvester impl.ing

---
+++ trunk/Source/Code2015/World/Harvester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Apoc3D.Scene;
+using Apoc3D.Graphics;
+using Apoc3D.Vfs;
+using Code2015.EngineEx;
+
+namespace Code2015.World
+{ 
+    class Harvester : DynamicObject
+    {
+
+        
+        public Harvester(RenderSystem rs)
+        {
+            FileLocation fl = FileSystem.Instance.Locate("cow.mesh", GameFileLocs.Model);
+
+            ModelL0 = new Model(ModelManager.Instance.CreateInstance(rs, fl));
+
+
+
+        }
+
+
+
+
+        public override bool IsSerializable
+        {
+            get { return false; ; }
+        }
+    }
+}
