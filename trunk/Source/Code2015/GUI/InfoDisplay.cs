6777aa6a411fd78f8fb027a1857ad292fd7a5675
canbitwell <canbitwell@184c9104-dd7f-11de-b0fd-b3f6642ff6c2>
2010-03-22 08:26:31 +0000

    info display impl.ing

--- trunk/Source/Code2015/GUI/InfoDisplay.cs
+++ trunk/Source/Code2015/GUI/InfoDisplay.cs
@@ -6,6 +6,9 @@ using Apoc3D.Graphics;
 using Apoc3D.MathLib;
 using Code2015.EngineEx;
 using Code2015.World;
+using Apoc3D.Vfs;
+using Code2015.GUI.Controls;
+using Apoc3D.Collections;
 
 namespace Code2015.GUI
 {
@@ -16,13 +19,32 @@ namespace Code2015.GUI
         RtsCamera camera;
 
         Font font;
+        FastList<ProgressBar> prgBars = new FastList<ProgressBar>();
 
         public CityInfoDisplay(GameScene scene, RenderSystem rs)
         {
             this.scene = scene;
             this.camera = scene.Camera;
-            this.font = FontManager.Instance.GetFont("def");
+            this.font = FontManager.Instance.GetFont("default");
             this.renderSys = rs;
+
+
+            FileLocation fl = FileSystem.Instance.Locate("ig_prgbar_cmp.tex", GameFileLocs.GUI);
+            Texture prgBg = UITextureManager.Instance.CreateInstance(fl);
+
+            fl = FileSystem.Instance.Locate("ig_prgbar_imp.tex", GameFileLocs.GUI);
+            Texture prgBg1 = UITextureManager.Instance.CreateInstance(fl);
+
+            for (int i = 0; i < 25; i++)
+            {
+                ProgressBar bar = new ProgressBar();
+               
+                bar.Height = 18;
+                bar.Width = 117;
+                bar.ProgressImage = prgBg;
+                bar.Background = prgBg1;
+                prgBars.Add(bar);
+            }
         }
 
         public void Render(Sprite sprite)
@@ -44,6 +66,13 @@ namespace Code2015.GUI
 
                 font.DrawString(sprite, cc.Name, scrnPos.X + 1, scrnPos.Y + 1, 20, DrawTextFormat.Center, (int)ColorValue.Black.PackedValue);
                 font.DrawString(sprite, cc.Name, scrnPos.X, scrnPos.Y, 20, DrawTextFormat.Center, -1);
+
+                if (i < prgBars.Count)
+                {
+                    prgBars[i].X = scrnPos.X;
+                    prgBars[i].X = scrnPos.Y - 30;
+                    prgBars[i].Render(sprite);
+                }
             }
         }
     }
@@ -55,13 +84,17 @@ namespace Code2015.GUI
         RtsCamera camera;
 
         Font font;
+        Texture background;
 
         public ResInfoDisplay(GameScene scene, RenderSystem rs)
         {
             this.scene = scene;
             this.camera = scene.Camera;
-            this.font = FontManager.Instance.GetFont("def");
+            this.font = FontManager.Instance.GetFont("default");
             this.renderSys = rs;
+
+            FileLocation fl = FileSystem.Instance.Locate("ig_resMeter.tex", GameFileLocs.GUI);
+            background = UITextureManager.Instance.CreateInstance(fl);
         }
 
         public void Render(Sprite sprite)
@@ -79,6 +112,9 @@ namespace Code2015.GUI
 
                 string title = res.Type.ToString();
                 Size strSize = font.MeasureString(title, 20, DrawTextFormat.Center);
+
+
+                sprite.Draw(background, scrnPos.X, scrnPos.Y, ColorValue.White);
 
                 font.DrawString(sprite, title, scrnPos.X + 1, scrnPos.Y + 1, 20, DrawTextFormat.Center, (int)ColorValue.Black.PackedValue);
                 font.DrawString(sprite, title, scrnPos.X, scrnPos.Y, 20, DrawTextFormat.Center, -1);
