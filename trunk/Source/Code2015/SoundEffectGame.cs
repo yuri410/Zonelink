df2ca9172a3f6b5b088849e92e494967d651db67
zhijinzj <zhijinzj@184c9104-dd7f-11de-b0fd-b3f6642ff6c2>
2010-04-10 20:07:23 +0000

    add class SoundEffectGame  SoundManager SoundObject

--- trunk/Source/Code2015/SoundEffectGame.cs
+++ trunk/Source/Code2015/SoundEffectGame.cs
@@ -3,57 +3,55 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 
 namespace Code2015
 {
     public class SoundEffectGame
     {
-        AudioEmitter emitter;
-        AudioListener listener;
-        ContentManager contentManager;
-        SoundEffect effect;
-        ContentManager cm;
-
-        public AudioEmitter Emitter
-        {
-            get { return emitter; }
-            set { emitter = value; }
-        }
-
+        SoundEffect soundEffect;
         public AudioListener Listener
         {
-            get { return listener; }
-            set { listener = value; }
+            get;
+            set;
         }
-
-
-        public SoundEffectGame(IServiceProvider service, string name)
+        public AudioEmitter Emitter
         {
-            emitter = new AudioEmitter();
-            listener = new AudioListener();
-            cm = new ContentManager(service);
-            cm.RootDirectory = "Sounds";
-            effect = cm.Load<SoundEffect>(name);
+            get;
+            set;
         }
-        //private void ReadXML()
-        //{
-        //    FileLocation fl = FileSystem.Instance.Locate("", GameFileLocs.Config);
-        //    Configuration conf = ConfigurationManager.Instance.CreateInstance(fl);
-        //    foreach (KeyValuePair<string, ConfigurationSection> c in conf)
-        //    {
-        //        ConfigurationSection sect = c.Value;
-        //        string name = sect.Name;
-        //        switch (name)
-        //        { 
-
-        //        }
-        //    }
-
-        //} 
-        public void Play()
+        public string Name
         {
-            effect.Play();
+            get;
+            set;
         }
+        public SoundEffectGame(ContentManager cm,string name)
+        {
+            soundEffect = cm.Load<SoundEffect>(name);
+            this.Name = name;
+            this.Listener = new AudioListener();
+            this.Emitter = new AudioEmitter();
+        }
+
+        //播放声音
+        public SoundEffectInstance Play()
+        {
+            return soundEffect.Play();
+        }
+        //播放3D声音
+        public SoundEffectInstance Play(Vector3 listener, Vector3 emitter)
+        {
+            this.Listener.Position = listener;
+            this.Emitter.Position = emitter;
+            return soundEffect.Play3D(this.Listener, this.Emitter);
+        }
+        public SoundEffectInstance Play(Vector3 listener, Vector3 emitter, float volume, float pitch, bool loop)
+        {
+            this.Listener.Position = listener;           
+            this.Emitter.Position = emitter;
+            return soundEffect.Play3D(this.Listener, this.Emitter, volume, pitch, loop);
+        }
+
 
     }
 }
---
+++ trunk/Source/Code2015/SoundObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+namespace Code2015
+{
+    public class SoundObject
+    {
+        SoundEffectGame soundEffectGame;
+        ContentManager contentManager;
+
+        public SoundEffectGame SoundEffectGame
+        {
+            get;
+            set;
+        }
+        public SoundObject()
+        { 
+        
+        }
+        public SoundObject(ContentManager cm,string name)
+        {
+            contentManager = cm;
+            
+        }
+    }
+}
