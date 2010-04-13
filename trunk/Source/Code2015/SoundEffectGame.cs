7b616499abad123959a7b272f162fbcde6c74e6b
canbitwell <canbitwell@184c9104-dd7f-11de-b0fd-b3f6642ff6c2>
2010-04-13 17:04:28 +0000

    sound effect system impl.

--- trunk/Source/Code2015/SoundEffectGame.cs
+++ trunk/Source/Code2015/SoundEffectGame.cs
@@ -1,87 +1,232 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Apoc3D;
+using Apoc3D.Config;
+using Apoc3D.MathLib;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Apoc3D.MathLib;
 using MXF = Microsoft.Xna.Framework;
+
 namespace Code2015
 {
+    public enum SoundEffectPart
+    {
+        Default = 0,
+        Fadein = 0,
+        Medium = 1,
+        Fadeout
+    }
     public class SoundEffectGame
     {
-        SoundEffect soundEffect;
-        public AudioListener Listener
+        SoundEffect[][] soundEffects;
+        AudioListener listener;
+
+        public float Volume
         {
             get;
-            set;
-        }
-        public AudioEmitter Emitter
-        {
-            get;
-            set;
+            private set;
         }
         public string Name
         {
             get;
-            set;
+            private set;
         }
-        ContentManager contentManager;
-        public SoundEffectGame(SoundManager  sm,string name)
+        public SoundType Type
         {
-            contentManager = sm.MakeContentManager();
-            soundEffect = contentManager.Load<SoundEffect>(name);
-            this.Name = name;
-            this.Listener = new AudioListener();
-            this.Emitter = new AudioEmitter();
+            get;
+            private set;
+        }
+        public SoundControl Control
+        {
+            get;
+            private set;
+        }
+        public bool IsLooped
+        {
+            get { return (Control & SoundControl.Loop) == SoundControl.Loop; }
+        }
+
+
+        ContentManager contentManager;
+
+        public SoundEffectGame(AudioListener listener, SoundManager sm, ConfigurationSection sect)
+        {
+            this.contentManager = sm.Content;
+            this.listener = new AudioListener();
+
+
+            string[] control = sect.GetStringArray("Control");
+            for (int i = 0; i < control.Length; i++)
+            {
+                control[i] = control[i].ToLowerInvariant();
+                switch (control[i])
+                {
+                    case "loop":
+                        Control |= SoundControl.Loop;
+                        break;
+                    case "random":
+                        Control |= SoundControl.Random;
+                        break;
+                    case "ambient":
+                        Control |= SoundControl.Ambient;
+                        break;
+                }
+            }
+            Volume = sect.GetSingle("Volume", 1);
+            Type = (SoundType)Enum.Parse(typeof(SoundType), sect.GetString("Type", SoundType.Normal.ToString()), true);
+            //this.soundEffect = contentManager.Load<SoundEffect>(sect.Name);
+            soundEffects = new SoundEffect[3][];
+
+            if ((Control & SoundControl.Loop) == SoundControl.Loop)
+            {
+                string[] fadein = sect.GetStringArray("Fadein");
+                soundEffects[(int)SoundEffectPart.Fadein] = new SoundEffect[fadein.Length];
+                for (int index = 0; index < fadein.Length; index++)
+                {
+                    soundEffects[(int)SoundEffectPart.Fadein][0] = contentManager.Load<SoundEffect>(fadein[index]);
+                }
+
+                string[] mediumstring = sect.GetStringArray("Medium");
+                soundEffects[(int)SoundEffectPart.Medium] = new SoundEffect[mediumstring.Length];
+                for (int index = 0; index < mediumstring.Length; index++)
+                {
+                    soundEffects[(int)SoundEffectPart.Medium][index] = contentManager.Load<SoundEffect>(mediumstring[index]);
+                }
+
+                string[] fadeout = sect.GetStringArray("Fadeout");
+                soundEffects[(int)SoundEffectPart.Fadeout] = new SoundEffect[fadeout.Length];
+                for (int index = 0; index < fadein.Length; index++)
+                {
+                    soundEffects[(int)SoundEffectPart.Fadeout][0] = contentManager.Load<SoundEffect>(fadeout[index]);
+                }
+            }
+            else
+            {
+                string[] sounds = sect.GetStringArray("Sounds");
+                soundEffects[(int)SoundEffectPart.Fadein] = new SoundEffect[sounds.Length];
+                for (int i = 0; i < sounds.Length; i++)
+                {
+                    soundEffects[0][i] = contentManager.Load<SoundEffect>(sounds[i]);
+                }
+                soundEffects[1] = soundEffects[0];
+                soundEffects[2] = soundEffects[0];
+            }
+        }
+
+        public SoundEffectInstance Play(SoundEffectPart category, SoundObject soundObj)
+        {
+            switch (Type)
+            {
+                case SoundType.Normal:
+                    Play(category, soundObj.Volume);
+                    break;
+                case SoundType.Player:
+                    if (soundObj.Parent.IsLocalHumanPlayer)
+                    {
+                        Play(category, soundObj.Volume);
+                    }
+                    break;
+                case SoundType.Global:
+                case SoundType.World:
+                    Play3D(category, soundObj.Emitter);
+                    break;
+            }
+            throw new NotSupportedException();
         }
 
         //播放声音
-        public SoundEffectInstance Play()
+        public SoundEffectInstance Play(SoundEffectPart category)
         {
-            return soundEffect.Play();
+
+            int idx = 0;
+            if ((Control & SoundControl.Random) == SoundControl.Random)
+            {
+                idx = Randomizer.GetRandomInt(soundEffects[(int)category].Length);
+            }
+            return soundEffects[(int)category][idx].Play(Volume);
+
+            //return soundEffect.Play();
         }
-        public SoundEffectInstance Play(float volume)
+        public SoundEffectInstance Play(SoundEffectPart category, float volume)
         {
-            return soundEffect.Play(volume);
+
+            int idx = 0;
+            if ((Control & SoundControl.Random) == SoundControl.Random)
+            {
+                idx = Randomizer.GetRandomInt(soundEffects[(int)category].Length);
+            }
+            return soundEffects[(int)category][idx].Play(volume * Volume);
+
         }
         //pitch,pan（-1左声道，1右声道）默认值设为0；
-        public SoundEffectInstance Play(float volume,float pitch,float pan,bool isLoop)
+        public SoundEffectInstance Play(SoundEffectPart category, float volume, float pitch, float pan, bool isLoop)
         {
-            return soundEffect.Play(volume, pitch, pan, isLoop);
+
+            int idx = 0;
+            if ((Control & SoundControl.Random) == SoundControl.Random)
+            {
+                idx = Randomizer.GetRandomInt(soundEffects[(int)category].Length);
+            }
+            return soundEffects[(int)category][idx].Play(volume * Volume, pitch, pan, isLoop);
+
         }
         //播放3D声音
-       
-        public SoundEffectInstance Play(Vector3 listener, Vector3 emitter)
+
+
+        public SoundEffectInstance Play3D(SoundEffectPart category, AudioEmitter emitter)
+        {
+            int idx = 0;
+            if ((Control & SoundControl.Random) == SoundControl.Random)
+            {
+                idx = Randomizer.GetRandomInt(soundEffects[(int)category].Length);
+            }
+            return Play(soundEffects[(int)category][idx], emitter);
+
+        }
+
+        public SoundEffectInstance Play3D(SoundEffectPart category, AudioEmitter emitter, float volume, float pitch, bool loop)
+        {
+            int idx = 0;
+            if ((Control & SoundControl.Random) == SoundControl.Random)
+            {
+                idx = Randomizer.GetRandomInt(soundEffects[(int)category].Length);
+            }
+            return Play(soundEffects[(int)category][idx], emitter, volume * Volume, pitch, loop);
+        }
+
+
+        SoundEffectInstance Play(SoundEffect sf, AudioEmitter emitter)
         {
             unsafe
             {
-                MXF.Vector3* tempPointer1 = (MXF.Vector3*)(&listener);
-                this.Listener.Position = *tempPointer1;
+                //MXF.Vector3* tempPointer1 = (MXF.Vector3*)(&listener);
+                //this.Listener.Position = *tempPointer1;
 
-                MXF.Vector3* tempPointer2 = (MXF.Vector3*)(&emitter);
-                this.Emitter.Position = *tempPointer2;
-               
-                return soundEffect.Play3D(this.Listener, this.Emitter);
+                //MXF.Vector3* tempPointer2 = (MXF.Vector3*)(&emitter);
+                //this.Emitter.Position = *tempPointer2;
+
+                return sf.Play3D(listener, emitter);
             }
         }
-    
-        public SoundEffectInstance Play(Vector3 listener, Vector3 emitter, float volume, float pitch, bool loop)
+
+        SoundEffectInstance Play(SoundEffect sf, AudioEmitter emitter, float volume, float pitch, bool loop)
         {
             unsafe
             {
-                MXF.Vector3* tempPointer1 = (MXF.Vector3*)(&listener);
-                this.Listener.Position = *tempPointer1;
+                //MXF.Vector3* tempPointer1 = (MXF.Vector3*)(&listener);
+                //this.Listener.Position = *tempPointer1;
 
-                MXF.Vector3* tempPointer2 = (MXF.Vector3*)(&emitter);
-                this.Emitter.Position = *tempPointer2;
-
-                this.Listener.Position = *tempPointer1;
-                this.Emitter.Position = *tempPointer2;
-                return soundEffect.Play3D(this.Listener, this.Emitter, volume, pitch, loop);
+                //MXF.Vector3* tempPointer2 = (MXF.Vector3*)(&emitter);
+                //this.Emitter.Position = *tempPointer2;
+                
+                //this.Listener.Position = *tempPointer1;
+                //this.Emitter.Position = *tempPointer2;
+                return sf.Play3D(listener, emitter, volume * Volume, pitch, loop);
             }
         }
-        
-        
-        
+
+
+
     }
 }
--- trunk/Source/Code2015/SoundManager.cs
+++ trunk/Source/Code2015/SoundManager.cs
@@ -1,43 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.Xna.Framework.Content;
-using Apoc3D.Vfs;
 using Apoc3D.Config;
+using Apoc3D.MathLib;
+using Apoc3D.Vfs;
 using Code2015.EngineEx;
 using Microsoft.Xna.Framework.Audio;
-using Apoc3D.MathLib;
+using Microsoft.Xna.Framework.Content;
+using XF = Microsoft.Xna.Framework;
 
 namespace Code2015
 {
+    [Flags]
+    public enum SoundControl
+    {
+        None = 0,
+        Loop = 1,
+        Random = 1 << 1,
+        Ambient = 1 << 2
+    }
+    public enum SoundType
+    {
+        Normal = 0,
+        Player,
+        World,
+        Global
+    }
 
     public class SoundManager
     {
-        public static readonly int TotalSounds = 10;//环境音效的种类
-
-      
         private static volatile SoundManager instance;
         private static object synRoot = new object();
+
+        public static SoundManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public static void Initialize(IServiceProvider service)
+        {
+            instance = new SoundManager(service, "Sounds", "soundEffect.xml");
+
+        }
+
+        AudioListener listener;
+        Dictionary<string, SoundEffectGame> sfxTable;
+
         /// <summary>
         /// 需要提供一个service,以及音效所在文件夹的根目录
         /// </summary>
         /// <param name="service"></param>
         /// <param name="root"></param>
-        private SoundManager(IServiceProvider service, string root,string xmlurl)
+        private SoundManager(IServiceProvider service, string root, string xmlurl)
         {
             Service = service;
             RootDirectory = root;
             XMLUrl = xmlurl;
-        }
-        public static SoundManager GetInstance(IServiceProvider service, string root,string Xmlurl)
-        {
-            if (instance == null)
-                lock (synRoot)
-                {
-                    if (instance == null)
-                        instance = new SoundManager(service, root,Xmlurl);
-                }
-            return instance;
+            listener = new AudioListener();
+
+            contentManager = new ContentManager(service);
+            FileLocation fl = FileSystem.Instance.Locate(this.XMLUrl, GameFileLocs.Config);
+            Configuration conf = ConfigurationManager.Instance.CreateInstance(fl);
+
+            sfxTable = new Dictionary<string, SoundEffectGame>(conf.Count);
+            foreach (KeyValuePair<string, ConfigurationSection> c in conf)
+            {
+                ConfigurationSection sect = c.Value;
+                SoundEffectGame sfx = new SoundEffectGame(listener, this, sect);
+
+                sfxTable.Add(sfx.Name, sfx);
+            }
         }
 
         public IServiceProvider Service
@@ -59,213 +93,35 @@ namespace Code2015
         /// ContentManager用于加载声音素材
         /// </summary>
         ContentManager contentManager;
-        /// <summary>
-        /// service 与 rootDirectory都与soundManager的一样
-        /// </summary>
-        /// <returns></returns>
-        public ContentManager MakeContentManager()
-        {
-            contentManager = new ContentManager(this.Service, this.RootDirectory);
-            return contentManager;
 
+        public ContentManager Content
+        {
+            get { return contentManager; }
         }
-        /// <summary>
-        /// 用于存储所有声音的名称
-        /// </summary>
-       
 
-        bool isloop = false;
-        public bool IsLoop
+
+        public SoundObject MakeSoundObjcet(string name, ISoundObjectParent parent,  float r, float min, float max)
         {
-            get { return isloop; }
-            set { isloop = value; }
-        }
-      
-        public string[,] SoundsName = new string[10,10];
-        public string[] ObjectName = new string[10];
-        public SoundObject SoundObject
-        {
-            get;
-            set;
-        }      
-        public void Initialize()
-        {
-           
-            FileLocation fl = FileSystem.Instance.Locate(this.XMLUrl, GameFileLocs.Config);
-            Configuration conf = ConfigurationManager.Instance.CreateInstance(fl);
-            foreach (KeyValuePair<string, ConfigurationSection> c in conf)
+            SoundEffectGame sfx = sfxTable[name];
+            switch (sfx.Type)
             {
-                ConfigurationSection sect = c.Value;
-
-                //获得所有环境声音的名称
-                int i = 0;
-                ObjectName[i] = c.Key.ToLowerInvariant();
-                
-
-                string control = sect.GetString("Control", string.Empty).ToLowerInvariant();
-
-                if (control.Contains("loop") || control.Contains("random"))
-                {
-                    string fadein = sect.GetString("Fadein", string.Empty).ToLowerInvariant();
-                    SoundsName[i, 0] = fadein;
-                    string[] mediumstring = sect.GetStringArray("Medium");
-                    for (int index = 0; index < mediumstring.Length; index++)
+                case SoundType.Global:
+                case SoundType.World:
+                    if ((sfx.Control & SoundControl.Loop) == SoundControl.Loop)
                     {
-                        SoundsName[i, index + 1] = mediumstring[index];
+                        return new AmbientLoopSoundObject(this, parent, sfx, r, min, max);
                     }
-                    string fadeout = sect.GetString("Fadeout", string.Empty).ToLowerInvariant();
-                    SoundsName[i, mediumstring.Length + 2] = fadeout;
-
-               
-                    
-                        this.isloop = true;
-                }
-                else if (control.Contains("none"))
-                {
-                    string[] mediumstring = sect.GetStringArray("Medium");
-                   
-                    for (int index = 0; index < mediumstring.Length; index++)
+                    else if ((sfx.Control & SoundControl.Ambient) == SoundControl.Ambient)
                     {
-                        SoundsName[i, index] = mediumstring[index];
+                        return new AmbientSoundObject(this, parent, sfx, r, min, max);
                     }
-                    this.isloop = false;
-                }
-
-                i++;
-
+                    return new Normal3DSoundObject(this, parent, sfx, r, min, max);                  
+                case SoundType.Normal:
+                case SoundType.Player:
+                    return new NormalSoundObject(this, parent, sfx);
             }
-            
+            throw new NotSupportedException();
         }
-        public SoundObject MakeSoundObjcet(string name,Vector3 position)
-        {
-            string objectname = "";
-            for (int i = 0; i < ObjectName.Length; i++)
-            { 
-                if(ObjectName[i].Contains(name))
-                    objectname=name;
-            }
-            return new SoundObject(this, name, position);                     
-        }
-
-        //public SoundEffectGame[] LoadSounds(string name)
-        //{
-        //    SoundEffectGame[] soundsEffect = new SoundEffectGame[MaxEffect];
-
-
-        //    for (int i = 0; i < MaxEffect; i++)
-        //    {
-        //        try
-        //        {
-        //            soundsEffect[i] = contentManager.Load<SoundEffectGame>(name);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            Console.WriteLine(e.Message + "加载失败");
-        //        }
-        //    }
-        //    return soundsEffect;
-        //}
-        /// <summary>
-        /// 将各个音效连接起来
-        /// </summary>
-        /// <param name="start">切入的音乐</param>
-        /// <param name="medium">中间循环的音乐</param>
-        /// <param name="end">淡出的音乐</param>
-        /// <param name="distance">摄像机和发声物体之间的距离</param>
-        /*
-          public void ConnectEffect(SoundEffectGame start, SoundEffectGame[] medium, SoundEffectGame end, float distance)
-          {
-              //离得远时的音效
-              if (distance > MaxDistance)
-              {
-                  SoundEffectInstance startInstance = start.Play();
-                  startInstance.Pause();
-                  startInstance.Volume = 0.3f;
-                  startInstance.Play();
-                  SoundEffectInstance mediumInstance;
-                  if (startInstance.State == SoundState.Stopped)
-                  {
-                      int i = 0;
-                      if (i < medium.Length)
-                      {
-                          mediumInstance = medium[i].Play();
-                          mediumInstance.Pause();
-                          mediumInstance.Volume = 0.3f;
-                          mediumInstance.Play();
-                          if (mediumInstance.State == SoundState.Stopped)
-                          {
-                              i++;
-                              mediumInstance = medium[i].Play();
-                              mediumInstance.Pause();
-                              mediumInstance.Volume = 0.3f;
-                              mediumInstance.Play();
-                          }
-                      }
-                  }
-
-              }
-              //靠近发声物体时的音效
-              else if (distance <= MaxDistance && distance >= MinDistance)
-              {
-                  SoundEffectInstance startInstance = start.Play();
-                  startInstance.Pause();
-                  startInstance.Volume = 0.8f;
-                  startInstance.Play();
-                  SoundEffectInstance mediumInstance;
-                  if (startInstance.State == SoundState.Stopped)
-                  {
-                      int i = 0;
-                      if (i < medium.Length)
-                      {
-                          mediumInstance = medium[i].Play();
-                          mediumInstance.Pause();
-                          mediumInstance.Volume = 0.8f;
-                          mediumInstance.Play();
-                          if (mediumInstance.State == SoundState.Stopped)
-                          {
-                              i++;
-                              mediumInstance = medium[i].Play();
-                              mediumInstance.Pause();
-                              mediumInstance.Volume = 0.8f;
-                              mediumInstance.Play();
-                          }
-                      }
-                  }
-              }
-              else if (distance < MinDistance)
-              {
-                  SoundEffectInstance startInstance = start.Play();
-                  startInstance.Pause();
-                  startInstance.Volume = 1.0f;
-                  startInstance.Play();
-                  SoundEffectInstance mediumInstance;
-                  if (startInstance.State == SoundState.Stopped)
-                  {
-                      int i = 0;
-                      if (i < medium.Length)
-                      {
-                          mediumInstance = medium[i].Play();
-                          mediumInstance.Pause();
-                          mediumInstance.Volume = 1.0f;
-                          mediumInstance.Play();
-                          if (mediumInstance.State == SoundState.Stopped)
-                          {
-                              i++;
-                              mediumInstance = medium[i].Play();
-                              mediumInstance.Pause();
-                              mediumInstance.Volume = 1.0f;
-                              mediumInstance.Play();
-                          }
-                      }
-                  }
-              }
-
-
-
-
-
-
-          }*/
     }
 
 }
